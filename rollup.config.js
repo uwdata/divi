645de02fadf704e5e0ff8cb31768edb83cf37950
Luke-S-Snyder <snyderluke8@gmail.com>
2022-02-09 18:17:03 -0800

    Update config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,36 @@
 import bundleSize from 'rollup-plugin-bundle-size';
 import { terser } from 'rollup-plugin-terser';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonJS from '@rollup/plugin-commonjs'
+
+// const external = [ 'd3-array' ];
+// const globals = { 'd3-array': 'd3' }
 
 export default [
   {
     input: 'src/index.js',
-    plugins: [ bundleSize() ],
+    // external,
+    plugins: [ 
+      bundleSize(),
+      nodeResolve(),
+      commonJS({
+        include: 'node_modules/**'
+      })
+    ],
     output: [
       {
         file: 'dist/automatic-interaction.mjs',
-        format: 'es'
+        format: 'es',
+        // globals
       },
       {
         file: 'dist/automatic-interaction.min.js',
         format: 'umd',
+        // globals,
         sourcemap: true,
-        plugins: [ terser({ ecma: 2018 }) ],
+        plugins: [ 
+          terser({ ecma: 2018 }),
+        ],
         name: 'AutomaticInteraction'
       }
     ]
