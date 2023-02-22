a4322a5cc34a95c2f55447b5ac7620582f6edcf7
Luke-S-Snyder <snyderluke8@gmail.com>
2023-02-22 03:48:42 -0800

    feat: Interaction menu

--- src/toolbar/menu.js
+++ src/toolbar/menu.js
@@ -1,8 +1,41 @@
-export async function createMenu(svg) {
-    const divParent = document.createElement('div');
-    svg.parentElement.insertBefore(divParent, svg);
+import { select } from "d3-selection";
+import navigateIcon from './icons/navigate.svg';
+import annotateIcon from './icons/annotate.svg';
+import filterIcon from './icons/filter.svg';
+import linkIcon from './icons/link.svg';
+import downloadIcon from './icons/download.svg';
+import brushIcon from './icons/brush.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-    const response = await fetch('src/toolbar/control.html');
-    divParent.innerHTML = await response.text();
-    divParent.appendChild(svg);
+export function createMenu(svg) {
+    const svgRect = svg._getBBox();
+    const div = select(document.body)
+        .append('div')
+        .attr('id', 'menu')
+        .classed('card', 'true')
+        .classed('bg-light', true)
+        .style('position', 'absolute')
+        .style('vertical-align', 'middle')
+        .style('top', (svgRect.top + 20) + 'px')
+        .style('left', (svgRect.right + 20) + 'px');
+
+    div.append('div')
+        .html(navigateIcon);
+
+    div.append('div')
+        .html(brushIcon);
+
+    div.append('div')
+        .html(filterIcon);
+    
+    div.append('div')
+        .classed('btn-secondary', true)
+        .html(linkIcon);
+        
+    div.selectAll('div')
+        .classed('btn', true)
+        .classed('m-1', true)
+        .selectAll('svg')
+        .attr('width', 20)
+        .attr('height', 20);
 }
