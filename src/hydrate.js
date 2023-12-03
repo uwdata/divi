f4031c5666a01fa6894802b9f7fa7f83c73810e7
luke-s-snyder <snyderluke8@gmail.com>
2023-12-03 12:35:13 -0800

    update: remove svg null check

--- src/hydrate.js
+++ src/hydrate.js
@@ -1,10 +1,10 @@
 import { orchestrate } from './orchestration/orchestrate.js';
 import { parseDataset } from './parsers/dataset-parser.js';
 
-export async function hydrate(svg, options={}) {
+export async function hydrate(svg, options = {}) {
     if (!svg) return;
     if (!Array.isArray(svg)) svg = [svg];
 
     svg = svg.map(d => typeof d === 'string' ? document.querySelector(d) : d);
-    if (svg) return orchestrate(svg, await parseDataset(options));
+    return orchestrate(svg, await parseDataset(options));
 }
