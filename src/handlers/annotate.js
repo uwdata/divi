304fd4d1cf517c376e21c621a40823a62b601124
luke-s-snyder <snyderluke8@gmail.com>
2023-12-03 12:37:12 -0800

    update: disable annotation for now

--- src/handlers/annotate.js
+++ src/handlers/annotate.js
@@ -1,51 +1,78 @@
+// import { select } from 'd3-selection';
+// import { annotation, annotationLabel } from 'd3-svg-annotation';
 
-export function annotate(state) {
-    bind(state);
-}
-
-// annotate.unbind = function() {
-//     document.removeEventListener('click', listener);
+// export function annotate(state, x, y, text) {
+//     bind(state, x, y, text);
 // }
 
-let bind = function(state) {
-    function listener (event) {
-        if (!state.interactions.annotate.flag || state.svg.parentElement.style['visibility'] === "hidden") return;
+// // annotate.unbind = function() {
+// //     document.removeEventListener('click', listener);
+// // }
 
-        let x_click = event.clientX - state.svg.getBoundingClientRect().left,
-            y_click = event.clientY - state.svg.getBoundingClientRect().top;
-        
-        if (x_click < state.xAxis.range[0] || y_click < state.yAxis.range[1]) return;
+// const bind = function(state, x, y, text) {
+//     function listener(event) {
+//         if (!state.interactions.annotate.flag || state.svg.parentElement.style.visibility === 'hidden') return;
 
-        let text = prompt('Annotation text:');
-        if (!text) return;
+//         const x_click = event.clientX - state.svg.getBoundingClientRect().left;
+//         const y_click = event.clientY - state.svg.getBoundingClientRect().top;
 
-        const annotations = [
-            {
-              note: {
-                label: text
-                // title: "d3.annotationLabel"
-              },
-              x: event.clientX - state.svg.getBoundingClientRect().left,
-              y: event.clientY - state.svg.getBoundingClientRect().top,
-              dy: 20,
-              dx: 20,
-              connector: {
-                end: "dot" // 'dot' also available
-              }
-            }]
+//         if (x_click < state.xAxis.range[0] || y_click < state.yAxis.range[1]) return;
 
-        const makeAnnotations = d3.annotation()
-            .type(d3.annotationLabel)
-            .annotations(annotations)
+//         const text = prompt('Annotation text:');
+//         if (!text) return;
 
-        d3.select("#" + state.svg.id)
-            .append("g")
-            .attr("class", "annotation-group")
-            .call(makeAnnotations)
-        
-        // var keys = (event.ctrlKey ? " ctrl " : "") + (event.shiftKey ? " shift " : "") + (event.altKey ? " alt " : "");
-        // document.getElementById("logfile").innerHTML += event.type + " [" + keys + "] " + SVG.state().svg.id + " to annotate <br/>";
-    }  
+//         const annotations = [
+//             {
+//                 note: {
+//                     label: text
+//                 // title: "d3.annotationLabel"
+//                 },
+//                 x: event.clientX - state.svg.getBoundingClientRect().left,
+//                 y: event.clientY - state.svg.getBoundingClientRect().top,
+//                 dy: 20,
+//                 dx: 20,
+//                 connector: {
+//                     end: 'dot' // 'dot' also available
+//                 }
+//             }];
 
-    document.addEventListener('mouseup', listener);
-}
+//         const makeAnnotations = d3.annotation()
+//             .type(d3.annotationLabel)
+//             .annotations(annotations);
+
+//         d3.select('#' + state.svg.id)
+//             .append('g')
+//             .attr('class', 'annotation-group')
+//             .call(makeAnnotations);
+
+//         // var keys = (event.ctrlKey ? " ctrl " : "") + (event.shiftKey ? " shift " : "") + (event.altKey ? " alt " : "");
+//         // document.getElementById("logfile").innerHTML += event.type + " [" + keys + "] " + SVG.state().svg.id + " to annotate <br/>";
+//     }
+//     const annotations = [
+//         {
+//             note: {
+//                 label: text
+//             // title: "d3.annotationLabel"
+//             },
+//             //   x: event.clientX - state.svg.getBoundingClientRect().left,
+//             //   y: event.clientY - state.svg.getBoundingClientRect().top,
+//             x: state.xAxis.scale(x),
+//             y: state.yAxis.scale(y),
+//             dy: 20,
+//             dx: 20,
+//             connector: {
+//                 end: 'dot' // 'dot' also available
+//             }
+//         }];
+
+//     const makeAnnotations = annotation()
+//         .type(annotationLabel)
+//         .annotations(annotations);
+
+//     select('#' + state.svg.id)
+//         .append('g')
+//         .attr('class', 'annotation-group')
+//         .call(makeAnnotations);
+
+//     // document.addEventListener('mouseup', listener);
+// };
