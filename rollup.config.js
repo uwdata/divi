035c60ba0fd9fa0230322b753d596d7640f18354
luke-s-snyder <snyderluke8@gmail.com>
2024-04-23 20:49:37 -0700

    chore: Move dev dependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -7,32 +7,32 @@ import postcss from 'rollup-plugin-postcss';
 import json from '@rollup/plugin-json';
 
 export default [
-  {
-    input: 'src/index.js',
-    plugins: [ 
-      bundleSize(),
-      nodeResolve({ browser: true }),
-      commonJS({
-        include: 'node_modules/**'
-      }),
-      svg(),
-      postcss({ extensions: [ '.css' ] }),
-      json()
-    ],
-    output: [
-      {
-        file: 'dist/divi.mjs',
-        format: 'es'
-      },
-      {
-        file: 'dist/divi.min.js',
-        format: 'umd',
-        sourcemap: true,
-        plugins: [ 
-          terser({ ecma: 2018 }),
+    {
+        input: 'src/index.js',
+        plugins: [
+            bundleSize(),
+            nodeResolve({ browser: true }),
+            commonJS({
+                include: 'node_modules/**'
+            }),
+            svg(),
+            postcss({ extensions: ['.css'] }),
+            json()
         ],
-        name: 'divi'
-      }
-    ]
-  }
+        output: [
+            {
+                file: 'dist/divi.mjs',
+                format: 'es'
+            },
+            {
+                file: 'dist/divi.min.js',
+                format: 'umd',
+                sourcemap: true,
+                plugins: [
+                    terser({ ecma: 2018 })
+                ],
+                name: 'divi'
+            }
+        ]
+    }
 ];
